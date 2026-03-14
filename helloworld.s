1859e8c895d2b30ac14843883139036b4388ba57
MachoNachoBot <55323382+MachoNachoBot@users.noreply.github.com>
2026-03-14 01:56:06 +0000

    Part B

--- helloworld.s
+++ helloworld.s
@@ -1,57 +1,42 @@
 	.file	"helloworld.c"
 	.text
-	.globl	a
+	.section	.text.startup,"ax",@progbits
+	.p2align 4
+	.globl	main
+	.type	main, @function
+main:
+.LFB23:
+	.cfi_startproc
+	endbr64
+	xorl	%eax, %eax
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	main, .-main
+	.globl	string
 	.data
-	.align 4
-	.type	a, @object
-	.size	a, 4
-a:
-	.long	1
-	.globl	c
-	.type	c, @object
-	.size	c, 1
-c:
-	.byte	97
+	.align 16
+	.type	string, @object
+	.size	string, 22
+string:
+	.string	"the bear ate the grub"
 	.globl	f
 	.align 4
 	.type	f, @object
 	.size	f, 4
 f:
 	.long	1067320848
-	.globl	string
-	.align 16
-	.type	string, @object
-	.size	string, 22
-string:
-	.string	"the bear ate the grub"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB0:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$0, -4(%rbp)
-	jmp	.L2
-.L3:
-	movl	a(%rip), %eax
-	movl	%eax, a(%rip)
-	addl	$1, -4(%rbp)
-.L2:
-	cmpl	$9, -4(%rbp)
-	jle	.L3
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
+	.globl	c
+	.type	c, @object
+	.size	c, 1
+c:
+	.byte	97
+	.globl	a
+	.align 4
+	.type	a, @object
+	.size	a, 4
+a:
+	.long	1
 	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
